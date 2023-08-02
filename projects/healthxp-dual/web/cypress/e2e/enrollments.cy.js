b6aff747f2d737612a963f32032b1ebfdc6604fe
Felipe Blanco <feeh.blanco@gmail.com>
2023-08-02 17:03:00 -0300

    test: aula 5.1 e 5.2 consumindo a API com cypress e custom commands

--- projects/healthxp-dual/web/cypress/e2e/enrollments.cy.js
+++ projects/healthxp-dual/web/cypress/e2e/enrollments.cy.js
@@ -3,7 +3,7 @@ import data from "../fixtures/enrollments.json";
 import enrollsPage from "../support/pages/EnrollsPage";
 
 describe("matriculas", () => {
-  it("deve por matricular um novo aluno", () => {
+  it("deve poder matricular um novo aluno", () => {
     const dataTest = data.create;
 
     cy.task("resetStudent", dataTest.student);
@@ -13,10 +13,28 @@ describe("matriculas", () => {
     enrollsPage.navbar.goToEnrolls();
     enrollsPage.goToForm();
     enrollsPage.selectItem("student", dataTest.student.name);
-    enrollsPage.selectItem("plan", dataTest.plan);
+    enrollsPage.selectItem("plan", dataTest.plan.name);
     enrollsPage.fillCard(dataTest.student);
     enrollsPage.submit();
 
     enrollsPage.popup.haveText("Matrícula cadastrada com sucesso.");
+  });
+
+  it.only("não deve criar matricula duplicada", () => {
+    const dataTest = data.duplicate;
+
+    cy.task("resetStudent", dataTest.student);
+    cy.createEnroll(dataTest);
+
+    cy.adminLogin();
+
+    enrollsPage.navbar.goToEnrolls();
+    enrollsPage.goToForm();
+    enrollsPage.selectItem("student", dataTest.student.name);
+    enrollsPage.selectItem("plan", dataTest.plan.name);
+    enrollsPage.fillCard(dataTest.student);
+    enrollsPage.submit();
+
+    enrollsPage.popup.haveText("O aluno já possui matrícula cadastrada!");
   });
 });
